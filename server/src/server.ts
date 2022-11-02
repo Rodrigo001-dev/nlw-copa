e1bdc378b9c4646043851ddf13b91ef58a17e6fe
Rodrigo <rodrigorael53@gmail.com>
2022-11-02 14:57:19 -0300

    creating all routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import { z } from 'zod';
 import { PrismaClient } from '@prisma/client';
+import ShortUniqueId from 'short-unique-id';
 
 const prisma = new PrismaClient({
   // com o esse log o prisma vai dar logs de todas as queries que são executadas
@@ -24,6 +26,39 @@ async function start() {
     const count = await prisma.pool.count();
 
     return { count };
+  });
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count();
+
+    return { count };
+  });
+
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count();
+
+    return { count };
+  });
+
+  fastify.post('/pools', async (request, response) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    });
+    
+    const { title } = createPoolBody.parse(request.body);
+
+    // vai criar um codigo de 6 caracteres
+    const generate = new ShortUniqueId({ length: 6 });
+    const code = String(generate()).toUpperCase();
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    });
+
+    return response.status(201).send({ code });
   });
 
   await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
