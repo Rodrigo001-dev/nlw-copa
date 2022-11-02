af1d2ad2fca0141e4604222dea71eaedb4f0b22d
Rodrigo <rodrigorael53@gmail.com>
2022-11-02 01:07:32 -0300

    creating a Loading component

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-
 import { NativeBaseProvider, Center, Text } from 'native-base';
+import { 
+  useFonts, 
+  Roboto_400Regular, 
+  Roboto_500Medium, 
+  Roboto_700Bold 
+} from '@expo-google-fonts/roboto';
+
+import { THEME } from './src/styles/theme';
 
 export default function App() {
+  // o fontsLoaded vai me retornar se as fonts já foram carregadas no dispositivo
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
         <Text color="white" fontSize={24}>
           ola
         </Text>
