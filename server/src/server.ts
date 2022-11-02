603bbeae6aba180a8d4682297ef1a203cd986199
Rodrigo <rodrigorael53@gmail.com>
2022-11-01 23:48:44 -0300

    get the data from backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,7 +26,7 @@ async function start() {
     return { count };
   });
 
-  await fastify.listen({ port: 3333, host: '0.0.0.0' });
+  await fastify.listen({ port: 3333, /*host: '0.0.0.0'*/ });
 };
 
 start();
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,20 @@
-export default function Home() {
+interface HomeProps {
+  count: number;
+};
+
+export default function Home(props: HomeProps) {
   return (
-    <h1>ola</h1>
+    <h1>Contagem: {props.count}</h1>
   )
 }
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:3333/pools/count');
+  const data = await response.json();
+
+  return {
+    props: {
+      count: data.count
+    }
+  };
+};
