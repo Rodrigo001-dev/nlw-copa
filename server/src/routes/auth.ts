87db139bcdf57384baaba37c53d11c6638009b89
Rodrigo <rodrigorael53@gmail.com>
2022-11-03 17:36:10 -0300

    adding googleId to users model

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,34 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+
 import { prisma } from "../lib/prisma";
 
 export async function authRoutes(fastify: FastifyInstance) {
-  
+  fastify.post('/users', async (request) => {
+    const createUserBody = z.object({
+      access_token: z.string(),
+    });
+
+    const { access_token } = createUserBody.parse(request.body);
+
+    const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const userData = await userResponse.json();
+
+    const userInfoSchema = z.object({
+      id: z.string(),
+      email: z.string().email(),
+      name: z.string(),
+      picture: z.string().url(),
+    });
+
+    const userInfo = userInfoSchema.parse(userData);
+
+    return { userInfo };
+  });
 };
