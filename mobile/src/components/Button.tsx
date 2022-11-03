99877606a4965a9e406a1649ecb5f59a228d7f94
Rodrigo <rodrigorael53@gmail.com>
2022-11-03 11:55:08 -0300

    adding all the components

--- mobile/src/components/Button.tsx
+++ mobile/src/components/Button.tsx
@@ -1,31 +1,34 @@
-import { Button as ButtonNativeBase, Text, IButtonProps } from 'native-base';
+import { Button as ButtonNativeBase, IButtonProps, Text } from 'native-base';
 
-interface ButtonProps extends IButtonProps {
+interface Props extends IButtonProps {
   title: string;
   type?: 'PRIMARY' | 'SECONDARY';
-};
+}
 
-export function Button({ title, type = 'PRIMARY', ...rest }: ButtonProps) {
+export function Button({ title, type = 'PRIMARY', ...rest }: Props) {
   return (
-    <ButtonNativeBase 
+    <ButtonNativeBase
       w="full"
       h={14}
       rounded="sm"
       fontSize="md"
       textTransform="uppercase"
-      bg={type === 'SECONDARY' ? 'red.500' : 'yellow.500'}
+      bg={type === 'SECONDARY' ? 'red.500' : "yellow.500"}
       _pressed={{
-        bg: type === 'SECONDARY' ? 'red.600' : 'yellow.600'
+        bg: type === 'SECONDARY' ? "red.400" : "yellow.600"
+      }}
+      _loading={{
+        _spinner: { color: "black" }
       }}
       {...rest}
     >
       <Text
         fontSize="sm"
         fontFamily="heading"
-        color={type === 'SECONDARY' ? 'white' : 'black'}
+        color={type === 'SECONDARY' ? 'white' : "black"}
       >
         {title}
       </Text>
-    </ButtonNativeBase>
+    </ButtonNativeBase >
   );
-};
+}
